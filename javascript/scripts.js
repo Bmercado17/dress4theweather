6404864d09d9c721fdf016502160a5978f0ccb21
Bmercado17 <askotala@yahoo.com>
2019-03-28 06:08:23 -0700

    first commit to group project

---
+++ javascript/scripts.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function () {
+  $(".form-group").submit(function (event) {
+    var person1Input = $("input#degrees").val();
+
+    if (inputteddegreesInput <= 0) {
+      $("#snorkeling").show();
+    } else if (person1Input > 3 && person1Input <= 7) {
+      $("#cenote").show();
+    } else {
+      $("#quads").show();
+    }
+    event.preventDefault();
+  });
+});
