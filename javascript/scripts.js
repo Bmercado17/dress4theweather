14db03a142fbca073bdbd52a7907615628bc4707
wymerkd <wymerkd@gmail.com>
2019-04-02 19:00:08 -0700

    copy javascript from master to branch (kyle) in order to merge

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,32 @@
+//api code
+var getWeather = function(zipcode) {
+  $.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipcode + '&appid=' + apiKey).then(function(response) {
+    cTemp=((response.main.temp)-273.15)
+    fTemp=((cTemp)*(9/5)+32)
+    console.log(response)
+    console.log(fTemp);
+  }).fail(function(error) {
+    console.log(error.responseJSON.message);
+  });
+}
+
+
+/*
+http://api.openweathermap.org/data/2.5/weather?q=portland&appid=f5f64ba1d369197d4ef4f7b84d814759
+*/
+
+
+//user interface logic
+$(document).ready(function() {
+  $("form#zipcode-enter").submit(function(event){
+    var zipcode = parseInt($("input#zip").val());
+    getWeather(zipcode);
+    // alert(zipcode);
+    event.preventDefault();
+  })
+})
+
+
 
 // $(document).ready(function () {
 //   $(".form-group").submit(function (event) {
